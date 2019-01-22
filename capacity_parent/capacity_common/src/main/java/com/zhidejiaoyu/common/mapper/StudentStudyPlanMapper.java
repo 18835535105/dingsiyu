a38de82740477abaa5236fdee6487d1ef4b217d1
刘茂宇 <1272464982@qq.com>
2019-01-22 13:47:51 +0800

    添加mapper

---
+++ capacity_parent/capacity_common/src/main/java/com/zhidejiaoyu/common/mapper/StudentStudyPlanMapper.java
@@ -0,0 +1,17 @@
+package com.zhidejiaoyu.common.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.zhidejiaoyu.common.pojo.StudentStudyPlan;
+
+/**
+ * <p>
+ * 学生学习计划表 Mapper 接口
+ * </p>
+ *
+ * @author zdjy
+ * @since 2019-01-15
+ */
+public interface StudentStudyPlanMapper extends BaseMapper<StudentStudyPlan> {
+
+
+}
---
+++ capacity_parent/capacity_common/src/main/java/com/zhidejiaoyu/common/mapper/TeacherCourseLibraryMapper.java
@@ -0,0 +1,17 @@
+package com.zhidejiaoyu.common.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.zhidejiaoyu.common.pojo.TeacherCourseLibrary;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author zdjy
+ * @since 2019-01-15
+ */
+public interface TeacherCourseLibraryMapper extends BaseMapper<TeacherCourseLibrary> {
+
+
+}
