8c5ddb9c17c366a5f5ad8637af0da6b9c5d065fa
刘茂宇 <1272464982@qq.com>
2019-05-10 18:10:12 +0800

    修改

--- capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/service/impl/TeksServiceImpl.java
+++ capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/service/impl/TeksServiceImpl.java
@@ -277,6 +277,9 @@ public class TeksServiceImpl extends BaseServiceImpl<TeksMapper, Teks> implement
         // 学生所有课程id及课程名
         /* List<Map<String, Object>> courses = courseMapper.selectTextCourseIdAndCourseNameByStudentId(studentId);*/
         List<Map<String, Object>> courses = studentStudyPlanMapper.selByStudentId(studentId, 3);
+        if (courses == null || courses.size() == 0) {
+            return ServerResponse.createByError(400,"当前学生没有课程，请让老师添加");
+        }
         // 学生课程下所有例句的单元id及单元名
         if (courses.size() > 0) {
             List<Long> courseIds = new ArrayList<>(courses.size());
