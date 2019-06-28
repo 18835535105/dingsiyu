745d9d208e6a82fe05278bbe6e2fef49a0f7dab4
walter <763396567@qq.com>
2019-06-28 14:35:16 +0800

    增加业务异常类；

---
+++ capacity_parent/capacity_common/src/main/java/com/zhidejiaoyu/common/exception/ServiceException.java
@@ -0,0 +1,50 @@
+package com.zhidejiaoyu.common.exception;
+
+import com.zhidejiaoyu.common.exception.Enum.ServiceExceptionEnum;
+
+/**
+ * 自定义异常类
+ *
+ * @author wuchenxi
+ * @date 2019-06-28
+ */
+public class ServiceException extends RuntimeException{
+
+    private Integer code;
+
+    private String message;
+
+    private ServiceException() {}
+
+    public ServiceException(ServiceExceptionEnum serviceExceptionEnum) {
+        this.code = serviceExceptionEnum.getCode();
+        this.message = serviceExceptionEnum.getMessage();
+    }
+
+    public ServiceException(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public ServiceException(String message) {
+        this.code = 500;
+        this.message = message;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
