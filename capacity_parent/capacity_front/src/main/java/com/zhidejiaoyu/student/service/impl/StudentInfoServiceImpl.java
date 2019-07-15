8240890a18ae82e013e02aa681532040b6b98865
walter <763396567@qq.com>
2019-07-15 14:06:51 +0800

    修复学习效率大于 100% 的问题；

--- capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/service/impl/StudentInfoServiceImpl.java
+++ capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/service/impl/StudentInfoServiceImpl.java
@@ -350,8 +350,8 @@ public class StudentInfoServiceImpl extends BaseServiceImpl<StudentMapper, Stude
             if (lastDuration != null && lastDuration.getLoginOutTime() != null) {
                 long timeDifference = System.currentTimeMillis() - lastDuration.getLoginOutTime().getTime();
                 if (timeDifference / 1000 < dto.getValid()) {
-                    log.warn("学生 [{} -{} - {}] 保存有效时长过大！classify=[{}], courseId=[{}], unitId=[{}], validTime=[{}s]",
-                            student.getId(), student.getAccount(), student.getStudentName(), dto.getClassify(), dto.getCourseId(), dto.getUnitId(), dto.getValid());
+                    log.warn("学生 [{} -{} - {}] 保存有效时长过大！classify=[{}], courseId=[{}], unitId=[{}], validTime=[{}s], 实际最大可保存为[{}s]",
+                            student.getId(), student.getAccount(), student.getStudentName(), dto.getClassify(), dto.getCourseId(), dto.getUnitId(), dto.getValid(), timeDifference / 1000);
                     return timeDifference / 1000;
                 }
             }
