55b6440c6073ad8cee7a4850877d5028343a06b4
lmy <1272464982@qq.com>
2019-07-16 16:08:50 +0800

    添加星球进入设置

--- capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/service/impl/LoginServiceImpl.java
+++ capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/service/impl/LoginServiceImpl.java
@@ -1034,6 +1034,17 @@ public class LoginServiceImpl extends BaseServiceImpl<StudentMapper, Student> im
                 isHave = true;
             }
         }
+        if (type.equals(5)) {
+            Integer role = student.getRole();
+            if (role.equals(4)) {
+                isHave = true;
+            } else {
+                if (student.getRank() < 7) {
+                    isHave = true;
+                }
+                isHave=true;
+            }
+        }
         map.put("isHave", isHave);
         return ServerResponse.createBySuccess(map);
     }
