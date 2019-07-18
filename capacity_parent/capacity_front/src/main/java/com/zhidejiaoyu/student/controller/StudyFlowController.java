a47cf14f93be5d0b08dc342fd0b1a88bf10cc0e4
walter <763396567@qq.com>
2019-07-18 11:52:32 +0800

    修复从流程 2 退回到流程 1 时，反复学习的问题；

--- capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/controller/StudyFlowController.java
+++ capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/controller/StudyFlowController.java
@@ -1,13 +1,13 @@
 package com.zhidejiaoyu.student.controller;
 
 import com.zhidejiaoyu.common.utils.server.ServerResponse;
+import com.zhidejiaoyu.student.dto.NodeDto;
 import com.zhidejiaoyu.student.service.StudyFlowService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import javax.naming.Name;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -24,16 +24,13 @@ public class StudyFlowController {
     /**
      * 节点学完, 把下一节点初始化到student_flow表, 并把下一节点返回
      *
-     * @param courseId 课程id
-     * @param unitId 单元id
-     * @param nodeId 当前节点id
-     * @param grade 得分
      * @param isTrueFlow 是否执行正确情况的节点
      * @return  id 节点id
      *          modelName 节点模块名
      */
     @RequestMapping("/getNode")
-    public ServerResponse<Object> getNode(Long courseId, Long unitId, Long nodeId, Long grade, @RequestParam(required = false) String isTrueFlow, HttpSession session) {
-        return StudyFlowServic.getNode(courseId, unitId, nodeId, grade, isTrueFlow, session);
+    public ServerResponse<Object> getNode(NodeDto dto, @RequestParam(required = false) String isTrueFlow, HttpSession session) {
+        dto.setTrueFlow(isTrueFlow);
+        return StudyFlowServic.getNode(dto, isTrueFlow, session);
     }
 }
