be6f9e7ddc7c80a5ccdce5de81664d8d95b97cfe
walter <763396567@qq.com>
2019-07-24 14:02:14 +0800

    修复“冰火两重天”获取若干轮数据之后获取数据为空问题；

--- capacity_parent/capacity_front/src/test/java/com/zhidejiaoyu/common/mapper/SimpleVocabularyMapperTest.java
+++ capacity_parent/capacity_front/src/test/java/com/zhidejiaoyu/common/mapper/SimpleVocabularyMapperTest.java
@@ -1,11 +1,14 @@
 package com.zhidejiaoyu.common.mapper;
 
+import com.github.pagehelper.PageHelper;
 import com.zhidejiaoyu.BaseTest;
+import com.zhidejiaoyu.common.pojo.Vocabulary;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -28,5 +31,17 @@ public class SimpleVocabularyMapperTest extends BaseTest {
     @Test
     public void testSelectPictureWordFromLearned() {
         vocabularyMapper.selectPictureWordFromLearned(8956L, 3);
+    }
+
+    @Test
+    public void testSelectByPhaseNotInWord() {
+        Map<String, Object> map = new HashMap<>(16);
+        map.put("word", "hello");
+
+        List<Map<String, Object>> list = new ArrayList<>();
+        list.add(map);
+        PageHelper.startPage(1, 10);
+        List<Vocabulary> vocabularies = vocabularyMapper.selectByPhaseNotInWord("小学", list);
+        log.info("words=[{}]", vocabularies);
     }
 }
