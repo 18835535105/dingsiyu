7402111a7dff6c6b2a2f2e602a4e06ad5f5601c0
liu <1272464982@qq.com>
2019-09-20 14:27:24 +0800

    add

---
+++ capacity_parent/capacity_common/src/main/java/com/zhidejiaoyu/common/Vo/testbeforstudy/SmallCurrentUnitTest.java
@@ -0,0 +1,30 @@
+package com.zhidejiaoyu.common.Vo.testbeforstudy;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 小学本单元测试
+ *
+ * @author: wuchenxi
+ * @Date: 2019-09-16 16:11
+ */
+@Data
+@Builder
+@NoArgsConstructor
+public class SmallCurrentUnitTest implements Serializable {
+
+    /**
+     * 大标题类型
+     */
+    private Integer topic;
+
+    /**
+     *
+     */
+    private List<QuestionAndAnswer> questionAndAnswerList;
+}
