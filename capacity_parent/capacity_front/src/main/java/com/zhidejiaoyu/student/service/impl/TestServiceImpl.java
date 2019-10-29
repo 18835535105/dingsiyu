80e86285217e2ad09a0165823ee02adfaccee715
walter <763396567@qq.com>
2019-10-29 18:10:56 +0800

    删除无用注释；

--- capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/service/impl/TestServiceImpl.java
+++ capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/service/impl/TestServiceImpl.java
@@ -853,7 +853,6 @@ public class TestServiceImpl extends BaseServiceImpl<TestRecordMapper, TestRecor
         String phase = CommonMethod.getPhase(grade);
         String courseName;
 
-        // todo:以下取题以后改为从预科库中获取
         if ("初中".equals(phase)) {
             // 前往初中预科库查询简单单词
             vocabularies = vocabularyMapper.selectByStudentPhase(student, 1);
