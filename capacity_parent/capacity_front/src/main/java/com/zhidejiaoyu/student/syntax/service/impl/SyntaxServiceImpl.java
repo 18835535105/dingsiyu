91871b275ce2c6db1f1ece10974b8ab3c085dd7a
walter <763396567@qq.com>
2019-10-30 16:33:29 +0800

    增加@Service注解；

--- capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/syntax/service/impl/SyntaxServiceImpl.java
+++ capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/syntax/service/impl/SyntaxServiceImpl.java
@@ -4,6 +4,8 @@ import com.zhidejiaoyu.common.mapper.SyntaxTopicMapper;
 import com.zhidejiaoyu.common.pojo.SyntaxTopic;
 import com.zhidejiaoyu.student.service.impl.BaseServiceImpl;
 import com.zhidejiaoyu.student.syntax.service.SyntaxService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SyntaxServiceImpl extends BaseServiceImpl<SyntaxTopicMapper, SyntaxTopic> implements SyntaxService {
 }
