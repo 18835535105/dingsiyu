45626f0b6eb82a9b75039fc65a8878035e15e9b9
walter <763396567@qq.com>
2019-11-22 14:06:04 +0800

    修复获取单词本数据时参数中含有NaN报错问题；

--- capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/controller/BookController.java
+++ capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/controller/BookController.java
@@ -1,17 +1,16 @@
 package com.zhidejiaoyu.student.controller;
 
 import com.github.pagehelper.PageInfo;
+import com.zhidejiaoyu.common.Vo.bookVo.BookInfoVo;
 import com.zhidejiaoyu.common.Vo.bookVo.BookVo;
+import com.zhidejiaoyu.common.Vo.bookVo.PlayerVo;
 import com.zhidejiaoyu.common.pojo.Player;
 import com.zhidejiaoyu.common.utils.server.ServerResponse;
 import com.zhidejiaoyu.student.service.BookService;
-import com.zhidejiaoyu.common.Vo.bookVo.BookInfoVo;
-import com.zhidejiaoyu.common.Vo.bookVo.PlayerVo;
-import org.apache.commons.lang.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
@@ -27,7 +26,7 @@ import java.util.Base64;
 @RequestMapping("/book")
 public class BookController {
 
-    @Autowired
+    @Resource
     private BookService bookService;
 
     /**
@@ -46,37 +45,32 @@ public class BookController {
     /**
      * 获取单词本/句子本列表信息
      *
-     * @param session   session
-     * @param courseId 当前课程id
-     * @param unitId    单元id
+     * @param session    session
+     * @param unitId     单元id
      * @param studyModel 慧记忆，慧听写，慧默写，单词图鉴，例句听力，例句翻译，例句默写
-     * @param condition 查询条件 1：总单词（默认）；2：生词；3：熟词；4：剩余
+     * @param condition  查询条件 1：总单词（默认）；2：生词；3：熟词；4：剩余
      * @return
      */
     @GetMapping("/getWordBookList")
-    public ServerResponse<PageInfo<BookVo>> getWordBookList(HttpSession session, Long courseId, Long unitId, String studyModel,
-                                                            @RequestParam(name = "condition", defaultValue = "1") Integer condition, Integer pageNum,
-                                                            @RequestParam(name = "pageSize", defaultValue = "20") Integer pageSize) {
+    public ServerResponse<PageInfo<BookVo>> getWordBookList(HttpSession session, String unitId, String studyModel,
+                                                            @RequestParam(name = "condition", defaultValue = "1") Integer condition) {
         if (unitId == null) {
             return ServerResponse.createByErrorMessage("unitId can't be null!");
         }
         if (condition > 4 || condition < 1) {
             return ServerResponse.createByErrorMessage("condition 参数非法！");
         }
-        if (pageSize >= 100) {
-            return ServerResponse.createByErrorMessage("pageSize 数据过大！");
-        }
-        return bookService.getWordBookList(session, courseId, unitId, studyModel, condition, pageNum, pageSize);
+        return bookService.getWordBookList(session, unitId, studyModel, condition);
     }
 
     /**
      * 获取单词播放机播放内容和答案列表
      * 该接口的单词播放机适用于单词流程中学习单词播放机节点，不适用于单词本中单词播放机
      *
-     * @param session session
+     * @param session  session
      * @param courseId 当前课程id
-     * @param unitId  当前单元id
-     *                @param order 单词播放顺序(默认顺序播放) 1：顺序播放；2：随机播放；3：倒序播放
+     * @param unitId   当前单元id
+     * @param order    单词播放顺序(默认顺序播放) 1：顺序播放；2：随机播放；3：倒序播放
      * @return
      */
     @GetMapping("/getPlayer")
@@ -93,7 +87,7 @@ public class BookController {
      *
      * @param session
      * @param unitId
-     * @param type 播放机类型。	2：单词播放机（默认） 3：句子播放机
+     * @param type    播放机类型。	2：单词播放机（默认） 3：句子播放机
      * @param order
      * @return 单词播放顺序(默认顺序播放) 1：顺序播放；2：随机播放；3：倒序播放
      */
@@ -138,10 +132,10 @@ public class BookController {
     /**
      * 选中指定单词重新学习，将选中的单词置为生词
      *
-     * @param session    session信息
-     * @param courseId   课程id
-     * @param unitId     单元id
-     * @param wordIds    所选单词数组
+     * @param session  session信息
+     * @param courseId 课程id
+     * @param unitId   单元id
+     * @param wordIds  所选单词数组
      * @param classify 学习模块 （0=单词图鉴，1=慧记忆，2=慧听写，3=慧默写，4=例句听力，5=例句翻译，6=例句默写）
      * @return ServerResponse<String>
      */
@@ -160,13 +154,13 @@ public class BookController {
 
         final String encoded = Base64
                 .getEncoder()
-                .encodeToString( text.getBytes( StandardCharsets.UTF_8 ) );
-        System.out.println( encoded );
+                .encodeToString(text.getBytes(StandardCharsets.UTF_8));
+        System.out.println(encoded);
 
         final String decoded = new String(
-                Base64.getDecoder().decode( encoded ),
-                StandardCharsets.UTF_8 );
-        System.out.println( decoded );
+                Base64.getDecoder().decode(encoded),
+                StandardCharsets.UTF_8);
+        System.out.println(decoded);
 
     }
 
