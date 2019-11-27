820076dfafbb788336b0d4fa969ec7ffe7975daa
liu <1272464982@qq.com>
2019-11-27 15:24:49 +0800

    修改数据结构

--- capacity_parent/capacity_common/src/main/java/com/zhidejiaoyu/common/rank/RankOpt.java
+++ capacity_parent/capacity_common/src/main/java/com/zhidejiaoyu/common/rank/RankOpt.java
@@ -83,12 +83,13 @@ public class RankOpt {
         try {
             //删除学生金币信息
             redisTemplate.opsForZSet().remove(RankKeysConst.CLASS_GOLD_RANK + student.getTeacherId() + ":" + student.getClassId(), student.getId());
-            redisTemplate.opsForZSet().remove(RankKeysConst.SCHOOL_GOLD_RANK + student.getTeacherId() + ":" + student.getClassId(), student.getId());
-            redisTemplate.opsForZSet().remove(RankKeysConst.COUNTRY_GOLD_RANK + student.getTeacherId() + ":" + student.getClassId(), student.getId());
-           } catch (Exception e) {
+            redisTemplate.opsForZSet().remove(RankKeysConst.SCHOOL_GOLD_RANK + TeacherInfoUtil.getSchoolAdminId(student), student.getId());
+            redisTemplate.opsForZSet().remove(RankKeysConst.COUNTRY_GOLD_RANK, student.getId());
+        } catch (Exception e) {
             log.error("修改 redis 排行中的金币排行信息失败！[{} - {} - {}]", student.getId(), student.getAccount(), student.getStudentName(), e);
         }
     }
+
     /**
      * 删除膜拜排行信息
      *
@@ -98,12 +99,13 @@ public class RankOpt {
         try {
             //删除学生膜拜信息
             redisTemplate.opsForZSet().remove(RankKeysConst.CLASS_WORSHIP_RANK + student.getTeacherId() + ":" + student.getClassId(), student.getId());
-            redisTemplate.opsForZSet().remove(RankKeysConst.SCHOOL_WORSHIP_RANK + student.getTeacherId() + ":" + student.getClassId(), student.getId());
-            redisTemplate.opsForZSet().remove(RankKeysConst.COUNTRY_WORSHIP_RANK + student.getTeacherId() + ":" + student.getClassId(), student.getId());
+            redisTemplate.opsForZSet().remove(RankKeysConst.SCHOOL_WORSHIP_RANK + TeacherInfoUtil.getSchoolAdminId(student), student.getId());
+            redisTemplate.opsForZSet().remove(RankKeysConst.COUNTRY_WORSHIP_RANK, student.getId());
         } catch (Exception e) {
             log.error("修改 redis 排行中的金币排行信息失败！[{} - {} - {}]", student.getId(), student.getAccount(), student.getStudentName(), e);
         }
     }
+
     /**
      * 删除勋章排行信息
      *
@@ -113,12 +115,13 @@ public class RankOpt {
         try {
             //删除学生勋章信息
             redisTemplate.opsForZSet().remove(RankKeysConst.CLASS_MEDAL_RANK + student.getTeacherId() + ":" + student.getClassId(), student.getId());
-            redisTemplate.opsForZSet().remove(RankKeysConst.SCHOOL_MEDAL_RANK + student.getTeacherId() + ":" + student.getClassId(), student.getId());
-            redisTemplate.opsForZSet().remove(RankKeysConst.COUNTRY_MEDAL_RANK + student.getTeacherId() + ":" + student.getClassId(), student.getId());
+            redisTemplate.opsForZSet().remove(RankKeysConst.SCHOOL_MEDAL_RANK + TeacherInfoUtil.getSchoolAdminId(student), student.getId());
+            redisTemplate.opsForZSet().remove(RankKeysConst.COUNTRY_MEDAL_RANK, student.getId());
         } catch (Exception e) {
             log.error("修改 redis 排行中的金币排行信息失败！[{} - {} - {}]", student.getId(), student.getAccount(), student.getStudentName(), e);
         }
     }
+
     /**
      * 删除勋章证书信息
      *
@@ -128,8 +131,8 @@ public class RankOpt {
         try {
             //删除学生证书信息
             redisTemplate.opsForZSet().remove(RankKeysConst.CLASS_CCIE_RANK + student.getTeacherId() + ":" + student.getClassId(), student.getId());
-            redisTemplate.opsForZSet().remove(RankKeysConst.SCHOOL_CCIE_RANK + student.getTeacherId() + ":" + student.getClassId(), student.getId());
-            redisTemplate.opsForZSet().remove(RankKeysConst.COUNTRY_CCIE_RANK + student.getTeacherId() + ":" + student.getClassId(), student.getId());
+            redisTemplate.opsForZSet().remove(RankKeysConst.SCHOOL_CCIE_RANK + TeacherInfoUtil.getSchoolAdminId(student), student.getId());
+            redisTemplate.opsForZSet().remove(RankKeysConst.COUNTRY_CCIE_RANK, student.getId());
         } catch (Exception e) {
             log.error("修改 redis 排行中的金币排行信息失败！[{} - {} - {}]", student.getId(), student.getAccount(), student.getStudentName(), e);
         }
