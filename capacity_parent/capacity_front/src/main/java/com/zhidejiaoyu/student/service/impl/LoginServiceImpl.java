278e2ee69362514d3eff9edba6b531d5835fad4a
liu <1272464982@qq.com>
2019-12-09 15:19:34 +0800

    添加语法模块学习格式

--- capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/service/impl/LoginServiceImpl.java
+++ capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/service/impl/LoginServiceImpl.java
@@ -1107,10 +1107,16 @@ public class LoginServiceImpl extends BaseServiceImpl<StudentMapper, Student> im
         }
         //判断是否有语法课程
         if (type.equals(7)) {
-            int i = studentStudyPlanMapper.countByStudentId(student.getId(), type);
-            if (i > 0) {
-                isHave = true;
+            if (student.getSchoolName().contains("英语队长涿鹿鼓楼小学体验中心") ||
+                    student.getAccount().contains("2035") ||
+                    student.getAccount().contains("3557") ||
+                    student.getAccount().contains("3586")) {
+                int i = studentStudyPlanMapper.countByStudentId(student.getId(), type);
+                if (i > 0) {
+                    isHave = true;
+                }
             }
+
         }
         map.put("isHave", isHave);
         return ServerResponse.createBySuccess(map);
