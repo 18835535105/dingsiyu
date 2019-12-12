291b329a01d5371bd1940da1997b26ca82060d42
walter <763396567@qq.com>
2019-12-12 10:28:51 +0800

    增加学习模块接口模板；

---
+++ capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/learn/service/LearnService.java
@@ -0,0 +1,8 @@
+package com.zhidejiaoyu.student.learn.service;
+
+/**
+ * @author: wuchenxi
+ * @Date: 2019/12/12 10:22
+ */
+public interface LearnService {
+}
---
+++ capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/learn/service/impl/LearnServiceImpl.java
@@ -0,0 +1,14 @@
+package com.zhidejiaoyu.student.learn.service.impl;
+
+import com.zhidejiaoyu.student.learn.service.LearnService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author: wuchenxi
+ * @Date: 2019/12/12 10:23
+ */
+@Slf4j
+@Service
+public class LearnServiceImpl implements LearnService {
+}
