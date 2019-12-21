e0d0698bffa6bca4355008a824a97e1f3e9c6a19
wuchenxi <763396567@qq.com>
2019-12-21 14:00:30 +0800

    完善摸底测试题目获取逻辑；

---
+++ capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/business/test/constant/TestConstant.java
@@ -0,0 +1,15 @@
+package com.zhidejiaoyu.student.business.test.constant;
+
+/**
+ * 测试相关常量
+ *
+ * @author: wuchenxi
+ * @date: 2019/12/21 11:42:42
+ */
+public interface TestConstant {
+
+    /**
+     * 摸底测试中可参与测试的所有单元id，存放在session中，用于保存摸底测试记录推送课程时使用
+     */
+    String TEST_BEFORE_STUDY_UNIT_IDS = "TEST_BEFORE_STUDY_UNIT_IDS";
+}
