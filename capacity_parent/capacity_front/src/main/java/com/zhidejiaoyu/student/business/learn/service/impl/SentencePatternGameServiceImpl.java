ff69c40a94aba8fddf18ff149a3932259bccebcf
liu <1272464982@qq.com>
2020-02-04 14:20:31 +0800

    句型游戏完成

--- capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/business/learn/service/impl/SentencePatternGameServiceImpl.java
+++ capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/business/learn/service/impl/SentencePatternGameServiceImpl.java
@@ -40,8 +40,8 @@ public class SentencePatternGameServiceImpl extends BaseServiceImpl<LearnNewMapp
         Student student = getStudent(session);
         Long studentId = student.getId();
         List<Map<String, Object>> getMaps;
-        Map<String,Object> returnMap=new HashMap<>();
-        List<Map<String,Object>> returnList=new ArrayList<>();
+        Map<String, Object> returnMap = new HashMap<>();
+        List<Map<String, Object>> returnList = new ArrayList<>();
         //获取当前单元下的group
         LearnNew learnNews = learnNewMapper.selectByStudentIdAndUnitIdAndEasyOrHardAndModelType(studentId, unitId, easyOrHard, modelType);
         if (learnNews == null) {
@@ -51,7 +51,7 @@ public class SentencePatternGameServiceImpl extends BaseServiceImpl<LearnNewMapp
         }
         //获取题目
         List<Map<String, Object>> lookMaps = unitSentenceNewMapper.selectSentenceAndChineseByUnitIdAndGroup(unitId, group);
-        if(lookMaps.size()>0){
+        if (lookMaps.size() > 0) {
             if (lookMaps.size() < 5) {
                 getMaps = lookMaps;
                 while (getMaps.size() < 5) {
@@ -67,24 +67,43 @@ public class SentencePatternGameServiceImpl extends BaseServiceImpl<LearnNewMapp
                 getMaps = lookMaps.subList(0, 5);
             }
             //获取干扰项
-            List<Long> sentenIds=new ArrayList<>();
-            getMaps.forEach(map->{
+            List<Long> sentenIds = new ArrayList<>();
+            getMaps.forEach(map -> {
                 sentenIds.add(Long.parseLong(map.get("id").toString()));
             });
             //获取不在sentenIds中的干扰项
             List<String> strings = sentenceMapper.selectNotIds(sentenIds);
             StudentExpansion expansion = studentExpansionMapper.selectByStudentId(studentId);
-            if(expansion.getPhase()==null){
-                returnMap.put("phase","小学");
-            }else{
-                returnMap.put("phase",expansion.getPhase());
+            if (expansion.getPhase() == null) {
+                returnMap.put("phase", "小学");
+            } else {
+                returnMap.put("phase", expansion.getPhase());
             }
             Collections.shuffle(getMaps);
-            for(Map<String,Object> map:getMaps){
+            for (Map<String, Object> map : getMaps) {
                 String chinese = map.get("chinese").toString();
-
+                String replace = chinese.replace("*", "");
+                Map<String, Object> reMap = new HashMap<>();
+                reMap.put("subject", replace);
+                Collections.shuffle(strings);
+                List<String> answers = strings.subList(0, 3);
+                List<String> answersList = new ArrayList<>();
+                String sentence = map.get("sentence").toString().replace("#", " ").replace("$", "");
+                answersList.add(sentence);
+                answers.forEach(answer -> answersList.add(answer.replace("#", " ").replace("$", "")));
+                Map<String, Boolean> answerMap = new HashMap<>();
+                answersList.forEach(answer->{
+                    if(answer.equals(sentence)){
+                        answerMap.put(answer,true);
+                    }else{
+                        answerMap.put(answer,false);
+                    }
+                });
+                reMap.put("answer", answerMap);
+                returnList.add(reMap);
             }
-            Collections.shuffle(strings);
+            returnMap.put("list", returnList);
+            return returnMap;
         }
 
         return null;
