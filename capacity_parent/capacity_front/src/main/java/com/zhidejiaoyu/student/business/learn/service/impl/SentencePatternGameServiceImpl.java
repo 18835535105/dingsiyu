f83c41f661b58696ba35aeaa429cda3cef7e41a8
liu <1272464982@qq.com>
2020-02-04 15:06:30 +0800

    句型游戏结构保存

--- capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/business/learn/service/impl/SentencePatternGameServiceImpl.java
+++ capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/business/learn/service/impl/SentencePatternGameServiceImpl.java
@@ -1,15 +1,18 @@
 package com.zhidejiaoyu.student.business.learn.service.impl;
 
-import com.zhidejiaoyu.common.mapper.LearnNewMapper;
-import com.zhidejiaoyu.common.mapper.SentenceMapper;
-import com.zhidejiaoyu.common.mapper.StudentExpansionMapper;
-import com.zhidejiaoyu.common.mapper.UnitSentenceNewMapper;
+import com.zhidejiaoyu.aliyunoss.common.AliyunInfoConst;
+import com.zhidejiaoyu.common.constant.PetMP3Constant;
+import com.zhidejiaoyu.common.constant.study.PointConstant;
+import com.zhidejiaoyu.common.mapper.*;
 import com.zhidejiaoyu.common.pojo.LearnNew;
 import com.zhidejiaoyu.common.pojo.Student;
 import com.zhidejiaoyu.common.pojo.StudentExpansion;
+import com.zhidejiaoyu.common.study.TestPointUtil;
+import com.zhidejiaoyu.common.utils.pet.PetSayUtil;
 import com.zhidejiaoyu.student.business.learn.service.IStudyService;
 import com.zhidejiaoyu.student.business.service.impl.BaseServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -30,7 +33,11 @@ public class SentencePatternGameServiceImpl extends BaseServiceImpl<LearnNewMapp
     @Resource
     private SentenceMapper sentenceMapper;
     @Resource
+    private StudentMapper studentMapper;
+    @Resource
     private StudentExpansionMapper studentExpansionMapper;
+    @Autowired
+    private PetSayUtil petSayUtil;
     private Integer modelType = 2;
     private Integer easyOrHard = 1;
 
@@ -92,11 +99,11 @@ public class SentencePatternGameServiceImpl extends BaseServiceImpl<LearnNewMapp
                 answersList.add(sentence);
                 answers.forEach(answer -> answersList.add(answer.replace("#", " ").replace("$", "")));
                 Map<String, Boolean> answerMap = new HashMap<>();
-                answersList.forEach(answer->{
-                    if(answer.equals(sentence)){
-                        answerMap.put(answer,true);
-                    }else{
-                        answerMap.put(answer,false);
+                answersList.forEach(answer -> {
+                    if (answer.equals(sentence)) {
+                        answerMap.put(answer, true);
+                    } else {
+                        answerMap.put(answer, false);
                     }
                 });
                 reMap.put("answer", answerMap);
@@ -111,6 +118,41 @@ public class SentencePatternGameServiceImpl extends BaseServiceImpl<LearnNewMapp
 
     @Override
     public Object saveStudy(HttpSession session, Long unitId, Long wordId, boolean isTrue, Integer plan, Integer total, Long courseId, Long flowId) {
-        return null;
+        Student student = getStudent(session);
+        Integer gold = 0;
+        Integer enger = 0;
+        if (total == 100) {
+            gold = 5;
+            enger = 3;
+        }
+        if (total >= 60) {
+            gold = 3;
+            enger = 2;
+        }
+        student.setSystemGold(student.getSystemGold() + gold);
+        student.setEnergy(student.getEnergy() + enger);
+        studentMapper.updateById(student);
+        super.saveRunLog(student, 4, "学生[" + student.getStudentName() + "]在游戏《" + "句型游戏"
+                + "》中奖励#" + gold + "#枚金币" + "单元id:" + unitId);
+        Map<String, Object> resultMap = new HashMap<>();
+        if (total < PointConstant.EIGHTY) {
+            resultMap.put("petName", petSayUtil.getMP3Url(student.getPetName(), PetMP3Constant.UNIT_TEST_LESS_EIGHTY));
+            resultMap.put("text", "很遗憾，闯关失败，再接再厉。");
+            resultMap.put("backMsg", new String[]{"别气馁，已经超越了", TestPointUtil.getPercentage(total), "的同学，继续努力吧！"});
+        } else if (total < PointConstant.NINETY) {
+            resultMap.put("petName", petSayUtil.getMP3Url(student.getPetName(), PetMP3Constant.UNIT_TEST_EIGHTY_TO_HUNDRED));
+            resultMap.put("text", "闯关成功，独孤求败！");
+            resultMap.put("backMsg", new String[]{"恭喜你，已经超过", TestPointUtil.getPercentage(total), "的同学，再接再励！"});
+        } else {
+            resultMap.put("petName", petSayUtil.getMP3Url(student.getPetName(), PetMP3Constant.UNIT_TEST_HUNDRED));
+            resultMap.put("text", "恭喜你刷新了纪录！");
+            resultMap.put("backMsg", new String[]{"恭喜你，已经超过", TestPointUtil.getPercentage(total), "的同学，再接再励！"});
+        }
+        resultMap.put("energy", enger);
+        resultMap.put("gold", gold);
+        resultMap.put("point", total);
+        resultMap.put("imgUrl", AliyunInfoConst.host + student.getPartUrl());
+
+        return resultMap;
     }
 }
