3fdd9229f74e2b882a66a9caeae889bca08267d1
liu <1272464982@qq.com>
2020-02-28 18:25:44 +0800

    修改数据

--- capacity_parent/capacity_common/src/main/java/com/zhidejiaoyu/common/mapper/StudentEquipmentMapper.java
+++ capacity_parent/capacity_common/src/main/java/com/zhidejiaoyu/common/mapper/StudentEquipmentMapper.java
@@ -1,17 +1,19 @@
 package com.zhidejiaoyu.common.mapper;
 
-import com.zhidejiaoyu.common.pojo.StudentEquipment;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.zhidejiaoyu.common.pojo.StudentEquipment;
 import org.apache.ibatis.annotations.Param;
 
 /**
  * <p>
- *  Mapper 接口
+ * Mapper 接口
  * </p>
  *
  * @author zdjy
  * @since 2020-02-27
  */
 public interface StudentEquipmentMapper extends BaseMapper<StudentEquipment> {
+
+    StudentEquipment selectByStudentIdAndEquipmentId(@Param("studentId") Long studentId,@Param("equipmentId") Long equipmentId);
 
 }
