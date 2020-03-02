6056bb66dfcc7bf0e0b252132f51d9bc47abed98
wuchenxi <763396567@qq.com>
2020-03-02 14:34:39 +0800

    完成飞船配置雷达图展示；

--- capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/business/shipconfig/util/CalculateUtil.java
+++ capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/business/shipconfig/util/CalculateUtil.java
@@ -1,23 +1,131 @@
 package com.zhidejiaoyu.student.business.shipconfig.util;
 
+import com.zhidejiaoyu.common.constant.test.GenreConstant;
+import com.zhidejiaoyu.common.mapper.DurationMapper;
+import com.zhidejiaoyu.common.mapper.LearnNewMapper;
+import com.zhidejiaoyu.common.mapper.TestRecordMapper;
+import com.zhidejiaoyu.student.business.shipconfig.vo.IndexVO;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+
 /**
  * 计算规则工具
  *
  * @author: wuchenxi
  * @date: 2020/3/2 10:55:55
  */
+@Component
 public class CalculateUtil {
 
+    private static LearnNewMapper learnNewMapper;
+
+    private static DurationMapper durationMapper;
+
+    private static TestRecordMapper testRecordMapper;
+
+    @Resource(name = "learnNewMapper")
+    private LearnNewMapper learnNewMapperTmp;
+
+    @Resource(name = "durationMapper")
+    private DurationMapper durationMapperTmp;
+
+    @Resource(name = "testRecordMapper")
+    private TestRecordMapper testRecordMapperTmp;
+
+    @PostConstruct
+    public void init() {
+        learnNewMapper = this.learnNewMapperTmp;
+        durationMapper = this.durationMapperTmp;
+        testRecordMapper = this.testRecordMapperTmp;
+
+    }
 
     /**
-     * 计算本周状态值（攻击百分比）
+     * 攻击状态
      *
-     * @param baseValue      基础值
-     * @param learnWordCount 最近7天学习单词数
-     * @param <T>
+     * @param baseValue              攻击基础值
+     * @param studentId
+     * @param beforeSevenDaysDateStr 7天前的日期
+     * @param endDateStr             当前日期
      * @return
      */
-    public static <T> String getWeekState(T baseValue, Integer learnWordCount) {
-        return String.valueOf((Double) baseValue * (0.2 + learnWordCount / 30));
+    public static int getAttack(Integer baseValue, Long studentId, String beforeSevenDaysDateStr, String endDateStr) {
+        int count = learnNewMapper.countLearnedWordCountByStartDateAndEndDate(studentId, beforeSevenDaysDateStr, endDateStr);
+        return Math.min((int) (baseValue * (0.2 + count * 1.0 / 30)), 3000);
     }
+
+    /**
+     * 耐久度状态    耐久度基础值*(50%+学习时长/8小时)
+     *
+     * @param baseValue              耐久度基础值
+     * @param studentId
+     * @param beforeSevenDaysDateStr 7天前的日期
+     * @param endDateStr             当前日期
+     * @return
+     */
+    public static int getDurability(Integer baseValue, Long studentId, String beforeSevenDaysDateStr, String endDateStr) {
+        Integer validTime = durationMapper.selectValidTime(studentId, beforeSevenDaysDateStr, endDateStr);
+        if (validTime == null) {
+            validTime = 0;
+        }
+        return Math.min((int) (baseValue * (0.5 + validTime * 1.0 / 28800)), 30000);
+    }
+
+    /**
+     * 命中率状态 命中百分比基础值*(50%+复习频次/7)
+     *
+     * @param baseValue              命中率基础值
+     * @param studentId
+     * @param beforeSevenDaysDateStr 7天前的日期
+     * @param endDateStr             当前日期
+     * @return
+     */
+    public static double getHitRate(Double baseValue, Long studentId, String beforeSevenDaysDateStr, String endDateStr) {
+        int count = testRecordMapper.countByGenreWithBeginTimeAndEndTime(studentId, GenreConstant.SMALLAPP_GENRE, beforeSevenDaysDateStr, endDateStr);
+        return Math.min(baseValue * (0.5 + count * 1.0 / 7), 2);
+    }
+
+    /**
+     * 机动力状态    机动力基础值* (50 % +有效时长 / 在线时长)
+     *
+     * @param baseValue              耐久度基础值
+     * @param studentId
+     * @param beforeSevenDaysDateStr 7天前的日期
+     * @param endDateStr             当前日期
+     * @return
+     */
+    public static int getMove(Integer baseValue, Long studentId, String beforeSevenDaysDateStr, String endDateStr) {
+        Integer onlineTime = durationMapper.selectOnlineTime(studentId, beforeSevenDaysDateStr, endDateStr);
+        Integer validTime = durationMapper.selectValidTime(studentId, beforeSevenDaysDateStr, endDateStr);
+        if (onlineTime == null) {
+            onlineTime = 0;
+        }
+        if (validTime == null) {
+            validTime = 0;
+        }
+        if (validTime > onlineTime) {
+            validTime = (int) (onlineTime * 0.98);
+        }
+
+        return Math.min((int) (baseValue * (0.5 + validTime * 1.0 / onlineTime)), 200);
+    }
+
+    /**
+     * 源力状态 源力攻击状态*源力
+     * 源力攻击状态=源力攻击基础值*(50%+本周平均成绩/100)
+     *
+     * @param baseValue
+     * @param studentId
+     * @param beforeSevenDaysDateStr
+     * @param endDateStr
+     * @return
+     */
+    public static int getSource(IndexVO.BaseValue baseValue, Long studentId, String beforeSevenDaysDateStr, String endDateStr) {
+        double avg = testRecordMapper.selectScoreAvgByStartDateAndEndDate(studentId, beforeSevenDaysDateStr, endDateStr);
+        return Math.min(30000, (int) (baseValue.getAttack() * (0.5 + avg * 1.0 / 100)));
+    }
+
+
 }
