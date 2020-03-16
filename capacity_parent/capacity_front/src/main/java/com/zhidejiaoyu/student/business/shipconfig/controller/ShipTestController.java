e4c9e080c08a14c149b032bb8cf9e8f81b9ea63a
liu <1272464982@qq.com>
2020-03-16 09:15:43 +0800

    增加飞船Pk内容

---
+++ capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/business/shipconfig/controller/ShipTestController.java
@@ -0,0 +1,9 @@
+package com.zhidejiaoyu.student.business.shipconfig.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("shipTest")
+public class ShipTestController {
+}
