c7520ecc03a92b04da5194f53a3fa2a00ef8f1d0
wuchenxi <763396567@qq.com>
2020-04-15 17:22:12 +0800

    增加微信小程序获取视频接口（测试使用，暂无逻辑）;

---
+++ capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/business/smallapp/controller/VideoController.java
@@ -0,0 +1,47 @@
+package com.zhidejiaoyu.student.business.smallapp.controller;
+
+import com.zhidejiaoyu.common.utils.server.ServerResponse;
+import com.zhidejiaoyu.student.business.smallapp.serivce.VideoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * 小程序视频接口
+ *
+ * @author: wuchenxi
+ * @date: 2020/4/15 16:51:51
+ */
+@RestController
+@RequestMapping("/smallApp/video")
+public class VideoController {
+
+    @Resource
+    private VideoService videoService;
+
+    /**
+     * 获取视频路径
+     *
+     * @param openId
+     * @return
+     */
+    @GetMapping("/getVideo")
+    public ServerResponse<Object> getVideo(String openId) {
+        return ServerResponse.createBySuccess("http://media.yydz100.com/sv/5116dc8a-170e26c49cb/5116dc8a-170e26c49cb.mp4");
+    }
+
+    /**
+     * 保存已观看视频信息
+     *
+     * @param openId
+     * @param gold   奖励的金币数
+     * @return
+     */
+    @PostMapping("/saveVideo")
+    public ServerResponse<Object> saveVideo(String openId, Integer gold) {
+        return ServerResponse.createBySuccess();
+    }
+}
---
+++ capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/business/smallapp/serivce/impl/VideoServiceImpl.java
@@ -0,0 +1,12 @@
+package com.zhidejiaoyu.student.business.smallapp.serivce.impl;
+
+import com.zhidejiaoyu.student.business.smallapp.serivce.VideoService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author: wuchenxi
+ * @date: 2020/4/15 16:53:53
+ */
+@Service
+public class VideoServiceImpl implements VideoService {
+}
