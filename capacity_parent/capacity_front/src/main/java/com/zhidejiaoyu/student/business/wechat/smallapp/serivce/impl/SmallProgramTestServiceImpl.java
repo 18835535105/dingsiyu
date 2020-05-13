93bd8dd8b4fc3968221bbb3053691a3aab4d30bd
liu <1272464982@qq.com>
2020-05-13 16:06:11 +0800

    修改二维码请求数据

--- capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/business/wechat/smallapp/serivce/impl/SmallProgramTestServiceImpl.java
+++ capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/business/wechat/smallapp/serivce/impl/SmallProgramTestServiceImpl.java
@@ -22,13 +22,13 @@ import com.zhidejiaoyu.student.business.wechat.smallapp.serivce.SmallProgramTest
 import com.zhidejiaoyu.student.business.wechat.smallapp.util.CreateWxQrCodeUtil;
 import com.zhidejiaoyu.student.common.GoldLogUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.propertyeditors.URIEditor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 import java.io.*;
+import java.net.URLEncoder;
 import java.util.*;
 
 /**
@@ -207,7 +207,7 @@ public class SmallProgramTestServiceImpl extends BaseServiceImpl<StudentMapper, 
     public Object getQRCode(String openId, String weChatName, String weChatImgUrl) {
 
         byte[] qrCode = CreateWxQrCodeUtil.createQRCode(GetLimitQRCodeDTO.builder()
-                .path("pages/support2/support?openid=" + openId + "&weChatName=" + weChatName + "&weChatImgUrl=" + weChatImgUrl)
+                .path( URLEncoder.encode("pages/support2/support?openid=" + openId + "&weChatName=" + weChatName + "&weChatImgUrl=" + weChatImgUrl))
                 .build());
 
         String fileName = System.currentTimeMillis() + ".png";
