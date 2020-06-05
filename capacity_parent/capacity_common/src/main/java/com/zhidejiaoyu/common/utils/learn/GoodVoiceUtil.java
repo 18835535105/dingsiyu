9112f4191875f1e7f26b7411d795ee2515db4e5d
wuchenxi <763396567@qq.com>
2020-06-05 12:16:18 +0800

    修复金币试卷空格与答案个数不相等问题；

--- capacity_parent/capacity_common/src/main/java/com/zhidejiaoyu/common/utils/learn/GoodVoiceUtil.java
+++ capacity_parent/capacity_common/src/main/java/com/zhidejiaoyu/common/utils/learn/GoodVoiceUtil.java
@@ -1,7 +1,7 @@
 package com.zhidejiaoyu.common.utils.learn;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.csp.ahas.shaded.com.alibaba.fastjson.JSONArray;
+import com.alibaba.csp.ahas.shaded.com.alibaba.fastjson.JSONObject;
 import com.zhidejiaoyu.common.utils.language.SpeechEvaluation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
