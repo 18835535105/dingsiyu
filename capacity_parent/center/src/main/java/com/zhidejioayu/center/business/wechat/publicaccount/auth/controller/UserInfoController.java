5437515da25cd0ac6cbfd01733df0e4122780f28
wuchenxi <763396567@qq.com>
2020-06-24 14:05:01 +0800

    增加项目前缀；

--- capacity_parent/center/src/main/java/com/zhidejioayu/center/business/wechat/publicaccount/auth/controller/UserInfoController.java
+++ capacity_parent/center/src/main/java/com/zhidejioayu/center/business/wechat/publicaccount/auth/controller/UserInfoController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 
@@ -31,7 +30,6 @@ public class UserInfoController {
      *
      * @return
      */
-    @ResponseBody
     @GetMapping("/getUserInfo")
     public ServerResponse<String> getUserInfo() {
         String code = HttpUtil.getHttpServletRequest().getParameter("code");
