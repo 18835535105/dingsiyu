9e078a9d98a8fae642b8d040ba585593e1a820a2
liu <1272464982@qq.com>
2020-06-29 12:41:45 +0800

    修改课程类获取数据格式

--- capacity_parent/course/src/main/java/com/dfdz/course/business/service/impl/TeksServiceImpl.java
+++ capacity_parent/course/src/main/java/com/dfdz/course/business/service/impl/TeksServiceImpl.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.dfdz.course.business.service.TeksService;
 import com.zhidejiaoyu.common.mapper.TeksMapper;
 import com.zhidejiaoyu.common.pojo.Teks;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TeksServiceImpl extends ServiceImpl<TeksMapper, Teks> implements TeksService {
 }
