c8a6d2a6d5d133f8e6ff2132501ffe8606772dc7
wuchenxi <763396567@qq.com>
2020-07-10 14:10:59 +0800

    企业微信增加过滤器；

--- capacity_parent/center/src/main/java/com/zhidejioayu/center/CenterApplication.java
+++ capacity_parent/center/src/main/java/com/zhidejioayu/center/CenterApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -17,6 +18,7 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @EnableScheduling
 @EnableCaching
 @EnableFeignClients
+@ServletComponentScan
 @ComponentScan(basePackages = {"com.zhidejiaoyu", "com.zhidejioayu.center", "com.dfdz.mail"})
 @MapperScan(basePackages = {"com.zhidejiaoyu.common.mapper"})
 @EnableRedisHttpSession(redisNamespace = "spring:session:center")
