c220333f87cac75ef163989775d753a8047ab907
wuchenxi <763396567@qq.com>
2020-07-23 11:01:24 +0800

    Revert "学习首页课程获取逻辑迁移到课程服务；"
    This reverts commit 5306869b

--- capacity_parent/course/src/main/java/com/dfdz/course/business/controller/CourseController.java
+++ capacity_parent/course/src/main/java/com/dfdz/course/business/controller/CourseController.java
@@ -1,17 +1,11 @@
 package com.dfdz.course.business.controller;
 
-import com.dfdz.course.business.service.CourseService;
-import com.dfdz.course.business.service.UnitService;
-import com.zhidejiaoyu.common.pojo.CourseNew;
-import com.zhidejiaoyu.common.pojo.UnitNew;
-import com.zhidejiaoyu.common.utils.StringUtil;
-import org.apache.commons.collections.CollectionUtils;
+import com.dfdz.course.business.service.*;
+import com.zhidejiaoyu.common.pojo.*;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author: wuchenxi
@@ -48,74 +42,11 @@ public class CourseController {
         return unitService.getUnitById(id);
     }
 
-    /**
-     * 根据课程id批量获取课程信息
-     *
-     * @param courseIds
-     * @return
-     */
-    @GetMapping("/getByIds")
-    public List<CourseNew> getByIds(@RequestParam List<Long> courseIds) {
-        if (CollectionUtils.isEmpty(courseIds)) {
-            return Collections.emptyList();
-        }
-        return courseService.listByIds(courseIds);
-    }
 
-    /**
-     * 获取学生可以学习的版本集合
-     *
-     * @param courseIds
-     * @return
-     */
-    @GetMapping("/getByIdsGroupByVersion")
-    List<CourseNew> getByIdsGroupByVersion(@RequestParam List<Long> courseIds) {
-        if (CollectionUtils.isEmpty(courseIds)) {
-            return Collections.emptyList();
-        }
-        return courseService.getByIdsGroupByVersion(courseIds);
-    }
 
-    /**
-     * 统计各个课程下单元个数
-     *
-     * @param courseIds
-     * @param type      1：单词；2：句型；3：语法；4：课文；5：金币试卷
-     * @return
-     */
-    @GetMapping("/countUnitByIds")
-    public Map<Long, Integer> countUnitByIds(@RequestParam List<Long> courseIds, @RequestParam int type) {
-        if (CollectionUtils.isEmpty(courseIds)) {
-            return Collections.emptyMap();
-        }
-        return courseService.countUnitByIds(courseIds, type);
-    }
 
-    /**
-     * 当前版本中小于或等于当前年级的所有课程id
-     *
-     * @param version   版本
-     * @param gradeList 年级
-     * @param type      1：单词；2：句型；3：语法；4：课文；5：金币试卷
-     * @return
-     */
-    @GetMapping("/getByGradeListAndVersionAndGrade")
-    public List<Long> getByGradeListAndVersionAndGrade(@RequestParam String version, @RequestParam List<String> gradeList, @RequestParam Integer type) {
-        if (StringUtil.isEmpty(version) || CollectionUtils.isEmpty(gradeList) || type == null) {
-            return Collections.emptyList();
-        }
-        return courseService.getByGradeListAndVersionAndGrade(version, gradeList, type);
-    }
 
-    /**
-     * 获取当前课程相关的语法数据
-     *
-     * @param courseNews
-     * @return
-     */
-    @PostMapping("/getByCourseNews")
-    public Map<Long, Map<String, Object>> getByCourseNews(@RequestBody List<CourseNew> courseNews) {
-        return courseService.getByCourseNews(courseNews);
-    }
+
+
 
 }
--- capacity_parent/course/src/main/java/com/dfdz/course/business/service/CourseService.java
+++ capacity_parent/course/src/main/java/com/dfdz/course/business/service/CourseService.java
@@ -3,48 +3,10 @@ package com.dfdz.course.business.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.zhidejiaoyu.common.pojo.CourseNew;
 
-import java.util.List;
-import java.util.Map;
-
 /**
  * @author: wuchenxi
  * @date: 2020/6/28 15:09:09
  */
 public interface CourseService extends IService<CourseNew> {
     CourseNew getCourseById(String id);
-
-    /**
-     * 统计各个课程下单元个数
-     *
-     * @param courseIds
-     * @param type      1：单词；2：句型；3：语法；4：课文；5：金币试卷
-     * @return
-     */
-    Map<Long, Integer> countUnitByIds(List<Long> courseIds, int type);
-
-    /**
-     * 当前版本中小于或等于当前年级的所有课程id
-     *
-     * @param version   版本
-     * @param gradeList 年级
-     * @param type      1：单词；2：句型；3：语法；4：课文；5：金币试卷
-     * @return
-     */
-    List<Long> getByGradeListAndVersionAndGrade(String version, List<String> gradeList, Integer type);
-
-    /**
-     * 获取当前课程的语法数据
-     *
-     * @param courseNews
-     * @return
-     */
-    Map<Long, Map<String, Object>> getByCourseNews(List<CourseNew> courseNews);
-
-    /**
-     * 根据课程id批量获取课程信息
-     *
-     * @param courseIds
-     * @return
-     */
-    List<CourseNew> getByIdsGroupByVersion(List<Long> courseIds);
 }
