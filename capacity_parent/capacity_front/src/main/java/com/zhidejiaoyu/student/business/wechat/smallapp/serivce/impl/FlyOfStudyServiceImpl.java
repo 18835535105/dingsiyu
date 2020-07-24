474ce616f862e46016afe8fdcc676340a5fee55b
wuchenxi <763396567@qq.com>
2020-07-24 15:17:53 +0800

    小程序通过扫码可以查看当天的学习记录；解决小程序时长统计冲突问题；

--- capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/business/wechat/smallapp/serivce/impl/FlyOfStudyServiceImpl.java
+++ capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/business/wechat/smallapp/serivce/impl/FlyOfStudyServiceImpl.java
@@ -4,11 +4,11 @@ import com.zhidejiaoyu.aliyunoss.getObject.GetOssFile;
 import com.zhidejiaoyu.common.mapper.*;
 import com.zhidejiaoyu.common.pojo.CurrentDayOfStudy;
 import com.zhidejiaoyu.common.pojo.Student;
-import com.zhidejiaoyu.common.utils.ArrayUtil;
 import com.zhidejiaoyu.common.utils.BigDecimalUtil;
 import com.zhidejiaoyu.common.utils.dateUtlis.DateUtil;
 import com.zhidejiaoyu.common.utils.server.ResponseCode;
 import com.zhidejiaoyu.common.utils.server.ServerResponse;
+import com.zhidejiaoyu.common.vo.currentdayofstudy.CurrentDayOfStudyVo;
 import com.zhidejiaoyu.student.business.currentDayOfStudy.service.CurrentDayOfStudyService;
 import com.zhidejiaoyu.student.business.service.impl.BaseServiceImpl;
 import com.zhidejiaoyu.student.business.wechat.smallapp.serivce.FlyOfStudyService;
@@ -18,9 +18,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author: wuchenxi
@@ -95,23 +93,49 @@ public class FlyOfStudyServiceImpl extends BaseServiceImpl<CurrentDayOfStudyMapp
             return ServerResponse.createByError(400, "未查询到当前二维码的学习信息！");
         }
 
+        String date = DateUtil.formatYYYYMMDD(currentDayOfStudy.getCreateTime());
+        if (Objects.equals(date, DateUtil.formatYYYYMMDD(new Date()))) {
+            // 查询当天的学习记录
+            ServerResponse<Object> response = currentDayOfStudyService.getCurrentDayOfStudy(student.getId());
+            if (response.getStatus() != ResponseCode.SUCCESS.getCode()) {
+                return response;
+            }
+            CurrentDayOfStudyVo vo = (CurrentDayOfStudyVo) response.getData();
+            return ServerResponse.createBySuccess(StudyInfoVO.builder()
+                    .contents(currentDayOfStudyService.getReturnList(this.joinStr(vo.getStudyModel())))
+                    .date(currentDayOfStudy.getCreateTime() == null ? "" : date)
+                    .errorSentence(currentDayOfStudyService.getTestList(this.joinStr(vo.getSentence())))
+                    .errorSyntax(currentDayOfStudyService.getReturnList(this.joinStr(vo.getSyntax())))
+                    .errorTest(currentDayOfStudyService.getTestList(this.joinStr(vo.getTest())))
+                    .errorText(currentDayOfStudyService.getReturnList(this.joinStr(vo.getText())))
+                    .errorWord(currentDayOfStudyService.getTestList(this.joinStr(vo.getWord())))
+                    .comment(currentDayOfStudy.getComment())
+                    .evaluate(currentDayOfStudy.getEvaluate())
+                    .siteNo(currentDayOfStudy.getSiteNo())
+                    .show(currentDayOfStudy.getShow())
+                    .studentName(student.getStudentName())
+                    .build());
+        }
+
+        // 非当天，指定日期的学习记录
         return ServerResponse.createBySuccess(StudyInfoVO.builder()
                 .contents(currentDayOfStudyService.getReturnList(currentDayOfStudy.getStudyModel()))
-                .date(currentDayOfStudy.getCreateTime() == null ? "" : DateUtil.formatYYYYMMDD(currentDayOfStudy.getCreateTime()))
+                .date(currentDayOfStudy.getCreateTime() == null ? "" : date)
                 .errorSentence(currentDayOfStudyService.getTestList(currentDayOfStudy.getSentence()))
                 .errorSyntax(currentDayOfStudyService.getReturnList(currentDayOfStudy.getSyntax()))
                 .errorTest(currentDayOfStudyService.getTestList(currentDayOfStudy.getTest()))
                 .errorText(currentDayOfStudyService.getReturnList(currentDayOfStudy.getText()))
                 .errorWord(currentDayOfStudyService.getTestList(currentDayOfStudy.getWord()))
-                .totalGold(currentDayOfStudy.getGold())
-                .totalOnlineTime(currentDayOfStudy.getOnlineTime())
-                .totalValidTime(currentDayOfStudy.getValidTime())
                 .comment(currentDayOfStudy.getComment())
                 .evaluate(currentDayOfStudy.getEvaluate())
                 .siteNo(currentDayOfStudy.getSiteNo())
                 .show(currentDayOfStudy.getShow())
                 .studentName(student.getStudentName())
                 .build());
+    }
+
+    private String joinStr(List<String> list) {
+        return list == null ? null : String.join("##", list);
     }
 
     @Override
