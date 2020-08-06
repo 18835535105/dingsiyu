fe27d86cbc9aeae88179a02d3dc72b6a2740cbac
wuchenxi <763396567@qq.com>
2020-08-06 13:40:32 +0800

    中台服务查询不到用户时抛出异常；

--- capacity_parent/capacity_front/src/test/java/com/zhidejiaoyu/student/business/feignclient/course/CourseFeignClientTest.java
+++ capacity_parent/capacity_front/src/test/java/com/zhidejiaoyu/student/business/feignclient/course/CourseFeignClientTest.java
@@ -2,6 +2,7 @@ package com.zhidejiaoyu.student.business.feignclient.course;
 
 import com.zhidejiaoyu.ZdjyFrontApplication;
 import com.zhidejiaoyu.common.pojo.CourseNew;
+import com.zhidejiaoyu.common.pojo.TeksNew;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author: wuchenxi
@@ -26,5 +28,11 @@ public class CourseFeignClientTest {
     public void testGetById() {
         CourseNew courseNew = courseFeignClient.getById(2749L);
         log.info("course={}", courseNew.toString());
+    }
+
+    @Test
+    public void selTeksByUnitIdAndGroup() {
+        List<TeksNew> teksNews = courseFeignClient.selTeksByUnitIdAndGroup(101033L, 1);
+        log.info("teksNews={}", teksNews.toString());
     }
 }
