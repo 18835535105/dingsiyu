dbbd5c2a276aa2e9663907e39c83277fe8b8d98f
walter <763396567@qq.com>
2020-09-07 13:49:38 +0800

    增加service;

---
+++ capacity_parent/course/src/main/java/com/dfdz/course/business/service/UnitTeksNewService.java
@@ -0,0 +1,11 @@
+package com.dfdz.course.business.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.zhidejiaoyu.common.pojo.UnitTeksNew;
+
+/**
+ * @author: wuchenxi
+ * @date: 2020/9/7 10:38:38
+ */
+public interface UnitTeksNewService extends IService<UnitTeksNew> {
+}
