4c5df2ced4fdd3831502a558acf3a12802ad997e
walter <763396567@qq.com>
2020-09-14 11:57:15 +0800

    解决项目无法启动问题；

--- capacity_parent/teacher/src/main/java/com/dfdz/teacher/TeacherApplication.java
+++ capacity_parent/teacher/src/main/java/com/dfdz/teacher/TeacherApplication.java
@@ -32,7 +32,7 @@ import java.util.Date;
 @ServletComponentScan
 @MapperScan(basePackages = {"com.zhidejiaoyu.common.mapper"})
 @EnableRedisHttpSession(redisNamespace = "spring:session:teacher")
-@ComponentScan(basePackages = {"com.zhidejiaoyu.common.config", "com.zhidejiaoyu.aliyunoss"}, basePackageClasses = {ControllerLogAop.class})
+@ComponentScan(basePackages = {"com.zhidejiaoyu"})
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class})
 public class TeacherApplication {
 
