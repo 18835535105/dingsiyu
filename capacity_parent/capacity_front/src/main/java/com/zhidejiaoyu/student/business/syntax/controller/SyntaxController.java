4a7c7bec71ef660b518365113f21bf23708aa6e0
dsy <dsy@test.com>
2020-09-18 11:50:44 +0800

    321123

--- capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/business/syntax/controller/SyntaxController.java
+++ capacity_parent/capacity_front/src/main/java/com/zhidejiaoyu/student/business/syntax/controller/SyntaxController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-
+import javax.annotation.Resource;
 /**
  * 超级语法
  *
